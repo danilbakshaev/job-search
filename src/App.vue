b97ce687ea39c38a95bf913d8861e1915c6e3630
danilbakshaev <bakshaevdanil@gmail.com>
2021-03-04 16:25:14 +0500

    Написал логику

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div v-if="currentJobs.length === 0"><p>Загрузка</p></div>
+    <div v-else>
+      <input type="text" v-model="search" placeholder="Search by city" />
+      {{ search }}
+      <br />
+      <div v-for="job in currentJobs" :key="job.id">{{ job.cities }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import getAllJobs from '@/mixins/getAllJobs';
 export default {
   name: 'App',
-  components: {},
+  data() {
+    return {
+      search: '',
+    };
+  },
+  mixins: [getAllJobs],
+  computed: {
+    currentJobs() {
+      const tempJobs = this.jobs || [];
+
+      // Если строка поиска не заполнена, тогда выводим пустой массив
+      if (!this.search) {
+        return tempJobs;
+      }
+
+      // Если строка поиска заполнена, то фильтруем объекты по ней
+      return tempJobs.filter((job) => {
+        let includeSearch = false;
+        job.cities.forEach((city) => {
+          if (city.name.toLowerCase().includes(this.search.toLowerCase())) {
+            includeSearch = true;
+          }
+        });
+        if (includeSearch) {
+          return job;
+        }
+      });
+    },
+  },
 };
 </script>
---
+++ src/mixins/getAllJobs.vue
@@ -0,0 +1,29 @@
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  apollo: {
+    jobs: gql`
+      query {
+        jobs {
+          id
+          title
+          tags {
+            name
+          }
+          remotes {
+            name
+          }
+          cities {
+            name
+          }
+          company {
+            logoUrl
+            name
+          }
+        }
+      }
+    `,
+  },
+};
+</script>
