fb958bacc222a66233974f45ede6dbce9bc0b6e2
danilbakshaev <bakshaevdanil@gmail.com>
2021-03-04 19:28:52 +0500

    Сделал верстку

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
-    <div v-if="currentJobs.length === 0"><p>Загрузка</p></div>
-    <div v-else>
-      <input type="text" v-model="search" placeholder="Search by city" />
-      {{ search }}
-      <br />
-      <div v-for="job in currentJobs" :key="job.id">{{ job.cities }}</div>
+    <div class="container mx-auto px-4 sm:px-0 mt-5">
+      <div v-if="currentJobs.length === 0">
+        <p class="text-center">Loading</p>
+      </div>
+      <div v-else class="bg-blue">
+        <t-input
+          v-model="search"
+          placeholder="Search by city"
+          name="my-input"
+        />
+        <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 mt-5">
+          <t-card v-for="job in currentJobs" :key="job.id" :header="job.title">
+            <img
+              v-if="job.company.logoUrl"
+              :src="job.company.logoUrl"
+              :alt="`Logo ${job.company.name}`"
+              class="card-img"
+            />
+            <div v-else class="card-noimg">No image</div>
+
+            <p class="my-3">Company: {{ job.company.name }}</p>
+
+            <p class="my-3" v-if="job.cities.length !== 0">
+              City:
+              <span v-for="(item, index) in job.cities" :key="index">{{
+                `${item.name} `
+              }}</span>
+            </p>
+
+            <p v-if="job.remotes.length !== 0" class="my-3">
+              Remote
+            </p>
+
+            <p class="my-3">
+              Tags:
+              <span v-for="(item, index) in job.tags" :key="index">{{
+                `${item.name} `
+              }}</span>
+            </p>
+          </t-card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import getAllJobs from '@/mixins/getAllJobs';
+import { TInput, TCard } from 'vue-tailwind/dist/components';
 export default {
   name: 'App',
   data() {
@@ -20,6 +57,7 @@ export default {
     };
   },
   mixins: [getAllJobs],
+  components: { TInput, TCard },
   computed: {
     currentJobs() {
       const tempJobs = this.jobs || [];
@@ -43,5 +81,29 @@ export default {
       });
     },
   },
+  settings: {
+    't-input': {
+      component: TInput,
+    },
+    't-card': {
+      component: TCard,
+    },
+  },
 };
 </script>
+
+<style scoped>
+.card-img {
+  max-width: 100px;
+  height: 100px;
+}
+.card-noimg {
+  width: 100px;
+  height: 100px;
+  background: rgb(209, 209, 209);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: grey;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './index.css';
-import { createProvider } from './vue-apollo'
+import { createProvider } from './vue-apollo';
+import VueTailwind from 'vue-tailwind';
 
+Vue.use(VueTailwind);
 Vue.config.productionTip = false;
 
 new Vue({
   apolloProvider: createProvider(),
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
